a85cdd3e246323cdb2537d675b72c43121ff7ae1
Ana <pganaclara@gmail.com>
2021-08-26 15:15:29 -0300

    modificacoes

--- tictactoe.vhd
+++ tictactoe.vhd
@@ -26,11 +26,9 @@ architecture behavioral of tictactoe is
            MOVE: out natural );
   end component;
 
-    -- The state of the unit
     type STATE_TYPE is (YMOVING, XMOVING);
     signal state: STATE_TYPE := YMOVING;
 
-  -- Our game board
   signal board: Grid := (others => EMPTY);
   signal winning_move: natural;
   signal blocking_move: natural;
@@ -52,47 +50,46 @@ begin
   process(CLK)
   begin
     if (rising_edge(CLK)) then
-      -- Reset the board
+      -- Resetar
       if (RESET = '1') then
-        report "RESET the board";
+        report "RESET";
         board <= (others => EMPTY);
         DRAW <= '0'; XWIN <= '0'; MOVEMADE <= '0';
       end if;
 
       case state is
         when YMOVING =>
-          -- Let Y make a move
+          -- Y faz o movimento
           MOVEMADE <= '0';
           if (MAKEMOVE = '1') then
-            report "Y's move is " & integer'image(ymove);
-            -- Y cannot make this move, set XMOVE to 0
+            report "O movimento escolhido foi " & integer'image(ymove);
+            -- Se não puder fazer XMOVE é 0
             if (board(YMOVE) /= EMPTY) then
               XMOVE <= 10; MOVEMADE <= '1';
             else
-              board(YMOVE) <= Y;        -- Make the move for Y
+              board(YMOVE) <= Y;        -- O jogador pode fazer o movimento.
               state <= XMOVING;
             end if;
           end if;
 
         when XMOVING =>
-			report "X moving, MOVEMADE is on";
-			report "Board is: ";
-			report "   " & Square'image(board(1)) & "  " & Square'image(board(2)) &	"  " & Square'image(board(3));
-			report "   " & Square'image(board(4)) & "  " & Square'image(board(5)) & "  " & Square'image(board(6));
-			report "   " & Square'image(board(7)) & "  " & Square'image(board(8)) & "  " & Square'image(board(9));
+			report "Tabuleiro: ";
+			report "   " & Square'image(board(1)) & " | " & Square'image(board(2)) & " | " & Square'image(board(3));
+			report "   " & Square'image(board(4)) & " | " & Square'image(board(5)) & " | " & Square'image(board(6));
+			report "   " & Square'image(board(7)) & " | " & Square'image(board(8)) & " | " & Square'image(board(9));
           MOVEMADE <= '1';
           state <= YMOVING;
 
-          -- See if we now have a move
+          -- Olha se tem como o PC ganhar ou bloquear o jogador
           if (winning_move /= 0) then
-            report "Winning move" & integer'image(winning_move);
+            report "Movimento ganhador" & integer'image(winning_move);
             XMOVE <= winning_move; board(winning_move) <= X;
             XWIN <= '1';
           elsif (blocking_move /= 0) then
-            report "Blocking move" & integer'image(blocking_move);
+            report "Movimento bloquador" & integer'image(blocking_move);
             XMOVE <= blocking_move; board(blocking_move) <= X;
 
-          -- No good move, so find any empty square
+          -- Se não tiver, vai pra qualquer vazio
           elsif (board(5) = EMPTY) then XMOVE <= 5; board(5) <= X; -- report "Move 5";
           elsif (board(1) = EMPTY) then XMOVE <= 1; board(1) <= X; -- report "Move 1";
           elsif (board(3) = EMPTY) then XMOVE <= 3; board(3) <= X; -- report "Move 3";
@@ -103,9 +100,9 @@ begin
           elsif (board(6) = EMPTY) then XMOVE <= 6; board(6) <= X; -- report "Move 6";
           elsif (board(8) = EMPTY) then XMOVE <= 8; board(8) <= X; -- report "Move 8";
 
-          -- No move at all, so declare a draw
+          -- Se não tiver movimento vazio e ninguém ganhou é empate
           else XMOVE <= 0; DRAW <= '1';
-               report "Draw";
+               report "Empate";
           end if;
       end case;
     end if;
